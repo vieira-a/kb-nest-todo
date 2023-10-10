e8fe77aac364f4c27839578b44716930f221ce17
Anderson Vieira <asvieira.br@gmail.com>
2023-10-10 18:21:23 -0300

    add postgres config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.service';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule, 
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
   controllers: [],
   providers: [],
 })
