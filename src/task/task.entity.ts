b34b8de8c1d5a80b1bbe6b19eab077818b45e11d
Anderson Vieira <asvieira.br@gmail.com>
2023-10-10 19:07:14 -0300

    migrate TaskEntity to TypeORM

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -1,7 +1,30 @@
+import { 
+  Entity, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn, 
+  PrimaryGeneratedColumn
+} from 'typeorm';
+
+Entity({ name: 'tasks' })
 export class TaskEntity {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column({ name: 'title', length: 100, nullable: false })
   title: string;
+
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
+
+  @Column({ name: 'status', nullable: false })
   status: 'open' | 'doing' | 'done';
+
+  @CreateDateColumn({ name: 'created_at'})
+  createdAt: string
+
+  @UpdateDateColumn({ name: 'updated_at'})
+  updatedAt: string
 }
 
