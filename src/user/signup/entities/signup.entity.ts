6af9c8042bff33e0f7e1cae08e86259e7fc8a3af
Anderson Vieira <asvieira.br@gmail.com>
2023-10-13 15:21:07 -0300

    feat: add SignUpEntity

---
+++ src/user/signup/entities/signup.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users_accounts'})
+export class SignUpEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name'})
+  name: string;
+
+  @Column({ name: 'username' })
+  username: string;
+
+  @Column({ name: 'email'})
+  email: string;
+
+  @Column({ name: 'password'})
+  password: string;
+
+  @Column({ name: 'created_at' })
+  createdAt: string;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: string;
+
+}
