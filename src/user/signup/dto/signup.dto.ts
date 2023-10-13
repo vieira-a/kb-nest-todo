57495818d19c772988e41aa72b2682faf40c6595
Anderson Vieira <asvieira.br@gmail.com>
2023-10-13 15:21:19 -0300

    feat: add SignUpDto

---
+++ src/user/signup/dto/signup.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty  } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio'})
+  name: string;
+
+  @IsNotEmpty({ message: 'O nome de usuário não pode ser vazio' })
+  username: string;
+
+  @IsNotEmpty({ message: 'O email não pode ser vazio'})
+  email: string;
+
+  @IsNotEmpty({ message: 'A senha precisa ser informada'})
+  password: string;
+
+  @IsNotEmpty({ message: 'A confirmação de senha precisa ser informada'})
+  passwordConfirmation: string;
+
+}
