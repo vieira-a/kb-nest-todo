9f3c71c8d7f725854c679713df48649a98d23b5b
Anderson Vieira <asvieira.br@gmail.com>
2023-10-17 18:25:32 -0300

    feat: ensure SignUpModule exports SignUpService to be used by LoginService

--- src/user/signup/signup.module.ts
+++ src/user/signup/signup.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SignUpEntity } from './entities/signup.entity';
 import { SignUpController } from './signup.controller';
 import { SignUpService } from './signup.service';
-import { BcryptAdapter } from '../utils/bcrypt-adapter';
+import { BcryptAdapter } from '../utils';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SignUpEntity])],
   controllers: [SignUpController],
   providers: [SignUpService, BcryptAdapter],
+  exports: [SignUpService],
 })
 export class SignUpModule {}
