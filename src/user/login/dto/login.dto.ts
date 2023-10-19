3dbd2c21cec59f1ade089b0a382214535635ab61
Anderson Vieira <asvieira.br@gmail.com>
2023-10-19 17:55:08 -0300

    refactor: export LoginDto as conventional class

--- src/user/login/dto/login.dto.ts
+++ src/user/login/dto/login.dto.ts
@@ -1,9 +1,6 @@
-import { IsString } from 'class-validator';
-
 export class LoginDto {
-  @IsString({ message: 'O nome de usuário deve ser informado' })
-  username: string;
-
-  @IsString({ message: 'A senha do usuário deve ser informada' })
-  password: string;
+  constructor(
+    readonly username: string,
+    readonly password: string
+  ) {}
 }
